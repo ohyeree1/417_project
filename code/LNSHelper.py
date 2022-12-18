57eddeb6647091233c9255b10888253b16553eba
alxwen711 <alxwen711@gmail.com>
2022-12-17 23:31:29 -0800

    LNS step 4+5, step 3 to be implemented, +refactoring

--- code/LNSHelper.py
+++ code/LNSHelper.py
@@ -22,3 +22,71 @@ def cost_of_path(p): #return the cost of a single given path
         else:
             c += p[k].edges[p[k+1].ID][1] #edge cost
     return c
+    
+def count_collisions(self, agent1, agent2): #return all collisions between two agents given their location tables
+    collisions = 0
+    end_time = max(agent1[1],agent2[1])
+    #collisions happening directly on a timestep 
+    for i in range(end_time):
+        a = self.get_location(agent1,i)
+        b = self.get_location(agent2,i)
+        if a == b and len(a) == 1 and len(b) == 1: #vertex collision
+            collisions += 1
+        elif len(a) == 2 and len(b) == 2: #possible edge traversal collision
+            if a[0] == b[1] and b[0] == a[1]: #edge traversal collision
+                collisions += 1 
+                """
+                side effect is that for long edges the collision 
+                may count multiple times, but does not affect the 
+                goal of having no collisions in the final solution
+                """
+        #if lengths differ then no collision as one is one a vertex
+        #and the other is traversing an edge
+
+    #collisions happening between two timesteps
+    #only possible case is when two agents swap nodes
+    #via an edge of cost 1, higher cost is caught by direct timestep
+
+    for j in range(end_time-1):
+        a = self.get_location(agent1,j)
+        aa = self.get_location(agent1,j+1)
+        b = self.get_location(agent2,j)
+        bb = self.get_location(agent2,j+1)
+        if max(len(a),len(b),len(aa),len(bb)) == 1 and a == bb and b == aa: #edge 1 collision
+            collisions += 1
+
+    return collisions
+
+def get_location(self,table,x): #return location at time x given table
+    if x >= table[1]: return table[0][table[1]] #agent has stopped moving
+    else: return table[0][x]
+
+def create_location_table(self,path): #return a dict for where an agent is based on "time"
+    #first value is a dictionary containing the locations, second value is an int for when the path ends 
+    ans = {}
+    time = 0
+    for i in range(len(path)-1):
+        ans[time] = [path[i].ID]
+        if path[i].ID == path[i+1].ID: #wait
+            time += 1
+        else: #traversing edge
+            edge_cost = path[i].edges[path[i+1].ID][1]
+            for j in range(time+1,time+edge_cost):
+                ans[j] = [path[i].ID,path[i+1].ID] #in process of traversing two nodes
+            time += edge_cost
+    ans[time] = [path[-1].ID] #last node
+    return [ans,time] #after time agent will be at last node
+
+def find_path(my_map,h,paths,start,finish,agent):
+    #returns a possible path for an agent based on a stocastic walk
+    """
+    TODO: refactor collision methods to be out of the LNS object
+    my_map -> full graph object
+    h -> heuristic function, enter any h[node] to get its cost
+    paths -> list of location dictionaries for each agent
+    start -> starting node
+    finish -> goal node (h[finish] == 0)
+    agent -> int value for the agent being fixed, ignore paths[agent] in collision detection
+    """
+    path = list()
+    return path
--- code/large_neighbourhood.py
+++ code/large_neighbourhood.py
@@ -9,14 +9,17 @@ python run_experiments.py --instance "testGraphs/graph*" --solver LNS
 
 above tests CBS on the test graphs and writes to output to CBSresults.txt
 swap CBS with other algorithms as needed
+
+other notes:
+astar returns a list of nodes, not node ids (see use of get_sum_of_costs)
 """
 class LargeNeighbourhoodSolver(object):
     """A planner that plans for each robot sequentially."""
 
     def __init__(self, graph):
         self.my_map = graph.nodeList #graph object containing the map
-        self.starts = list() #starting locations for each agent in order
-        self.goals = list() #ending locations for each agent in order
+        self.starts = list() #starting nodes for each agent in order
+        self.goals = list() #ending nodes for each agent in order
         self.num_of_agents = graph.agentCount #number of agents
         for i in range(self.num_of_agents):
             self.starts.append(graph.nodeList[graph.agents[i][0]])
@@ -24,64 +27,13 @@ class LargeNeighbourhoodSolver(object):
             
         self.CPU_time = 0
 
-        # compute Dijkstra heuristics
+        # compute Dijkstra heuristics, array of dicts with each corresponding 
+        # to the nth's agent goal being the "root" of the tree
         self.heuristics = []
         for goal in self.goals:
             self.heuristics.append(compute_heuristics(self.my_map, goal))
 
-    def count_collisions(self, agent1, agent2): #return all collisions between two agents given their location tables
-        collisions = 0
-        end_time = max(agent1[1],agent2[1])
-        #collisions happening directly on a timestep 
-        for i in range(end_time):
-            a = self.get_location(agent1,i)
-            b = self.get_location(agent2,i)
-            if a == b and len(a) == 1 and len(b) == 1: #vertex collision
-                collisions += 1
-            elif len(a) == 2 and len(b) == 2: #possible edge traversal collision
-                if a[0] == b[1] and b[0] == a[1]: #edge traversal collision
-                    collisions += 1 
-                    """
-                    side effect is that for long edges the collision 
-                    may count multiple times, but does not affect the 
-                    goal of having no collisions in the final solution
-                    """
-            #if lengths differ then no collision as one is one a vertex
-            #and the other is traversing an edge
-
-        #collisions happening between two timesteps
-        #only possible case is when two agents swap nodes
-        #via an edge of cost 1, higher cost is caught by direct timestep
-
-        for j in range(end_time-1):
-            a = self.get_location(agent1,j)
-            aa = self.get_location(agent1,j+1)
-            b = self.get_location(agent2,j)
-            bb = self.get_location(agent2,j+1)
-            if max(len(a),len(b),len(aa),len(bb)) == 1 and a == bb and b == aa: #edge 1 collision
-                collisions += 1
-
-        return collisions
-
-    def get_location(self,table,x): #return location at time x given table
-        if x >= table[1]: return table[0][table[1]] #agent has stopped moving
-        else: return table[0][x]
-
-    def create_location_table(self,path): #return a dict for where an agent is based on "time"
-        #first value is a dictionary containing the locations, second value is an int for when the path ends 
-        ans = {}
-        time = 0
-        for i in range(len(path)-1):
-            ans[time] = [path[i].ID]
-            if path[i].ID == path[i+1].ID: #wait
-                time += 1
-            else: #traversing edge
-                edge_cost = path[i].edges[path[i+1].ID][1]
-                for j in range(time+1,time+edge_cost):
-                    ans[j] = [path[i].ID,path[i+1].ID] #in process of traversing two nodes
-                time += edge_cost
-        ans[time] = [path[-1].ID] #last node
-        return [ans,time] #after time agent will be at last node
+    
 
     def find_solution(self):
         """ Finds paths for all agents from their start locations to their goal locations."""
@@ -98,35 +50,40 @@ class LargeNeighbourhoodSolver(object):
         # 1.5. Check if there are collisions, if there aren't any, then this setup is guaranteed to be optimal
         
         collisions = 0 #tracks TOTAL number of collisions
-        collision_pair_freq = list() #tracks number of collisions between a pair of agents
         collision_agent_freq = [0]*self.num_of_agents #tracks number of collisions an agent is involved in
-        for t in range(self.num_of_agents):
+        collision_pair_freq = list() #tracks number of collisions between a pair of agents
+        for this_varibale_will_not_be_used in range(self.num_of_agents):
             collision_pair_freq.append([0]*self.num_of_agents)
+
+        #create location tables for each agent
         agent_location = [{}]*self.num_of_agents
         for j in range(self.num_of_agents):
-            agent_location[j] = self.create_location_table(result[j])
+            agent_location[j] = create_location_table(result[j])
 
         #determine each collision occuring
         for m in range(self.num_of_agents):
             for n in range(m+1,self.num_of_agents):
-                pair_collisions = self.find_collisions(agent_location[m],agent_location[n])
+                pair_collisions = count_collisions(agent_location[m],agent_location[n])
                 collision_pair_freq[m][n] += pair_collisions
                 collision_pair_freq[n][m] += pair_collisions
                 collision_agent_freq[n] += pair_collisions
                 collision_agent_freq[m] += pair_collisions
                 collisions += pair_collisions
                 
-        if collisions == 0: #shortest path for each agent is optimal
+        if collisions == 0: #shortest path for each agent is optimal, can end early
             self.CPU_time = timer.time() - start_time
 
             print("\n Found a solution! \n")
+            print("LNS ended early due to Indpendent being an optimal solution.")
             print("CPU time (s):    {:.2f}".format(self.CPU_time))
             print("Sum of costs:    {}".format(get_sum_of_cost(result)))
-            #print("Paths in the solution:")
-            #print_paths(result)
-            #for j in range(self.num_of_agents):
-                #print("Agent #"+str(j),result[j])
-            #print(result)
+            """
+            print("Paths in the solution:")
+            print_paths(result)
+            for j in range(self.num_of_agents):
+                print("Agent #"+str(j),result[j])
+            print(result)
+            """
             return result
 
         #independent a* has problems, use stocastic search to fix
@@ -140,26 +97,57 @@ class LargeNeighbourhoodSolver(object):
         # 2. Choose the path stochastically with the most problems/or slowest and delete it
         # worst has 50%, 2nd worst 25%, 3rd worst 12.5%, so on until last two have equally small chance
         # For now assume the other paths are optimal
-        missCount = 0
-        while missCount < 5*self.num_of_agents: #stop stocastic search after certain number of attempts without improvement
+        miss_count = 0
+        while miss_count < 5*self.num_of_agents: #stop stocastic search after certain number of attempts without improvement
             path_ratings = list()
             for q in range(self.num_of_agents):
-                path_ratings.append([collision_agent_freq(q),result_costs[q],q])
+                path_ratings.append([collision_agent_freq[q],result_costs[q],q])
         
-        # order the paths from best to worst (via collisions involved in, then path cost), then pseudorandomly select one to fix
-            path_ratings.sort()
-            agent_to_fix = path_ratings[weighted_random(self.num_of_agents,0.3)][2] #fix result[agent_to_fix]
+            # order the paths from best to worst (via collisions involved in, then path cost), then pseudorandomly select one to fix
+                path_ratings.sort()
+                agent_to_fix = path_ratings[weighted_random(self.num_of_agents,0.3)][2] #fix result[agent_to_fix]
         
 
-        # 3. Recreate the path with a stocastic search
-        # Assign value to each action based on heuristic value
-        # Causes collision: 0 (unless all options result in collision)
-        # Optimal path according to heuristic has highest chance with suboptimal still possible since they may avoid collisions
+            # 3. Recreate the path with a stocastic search
+            # Assign value to each action based on heuristic value
+            # Path chosen ALWAYS greedingly chooses action that minimizes collisions
+            # A* value is used as tiebreaker, then order choices from best to worst
+            # Optimal path according to heuristic has highest chance with suboptimal still possible since they may avoid collisions
+            # Extra logic is used when waiting on goal node, prioritize waiting until a collision would occur, then move out
+            # minimum time = last collision with other path
 
+            atf = agent_to_fix #shorthand to make code easier to manage
+            new_path = find_path(self.my_map,self.heuristics[self.goals[atf]],agent_location,self.starts[atf],self.goals[atf],atf)
 
+            # 4. Reevaluate the solution, if better, keep it
+            np_collisions = list()
+            new_table = create_location_table(new_path)
+            for y in range(self.num_of_agents):
+                if y != agent_to_fix:
+                    np_collisions.append(count_collisions(new_table,agent_location[y]))
+                else:
+                    new_path.collisions.append(0)
+            np_cost = cost_of_path(new_path)
+            np_collision_count = sum(np_collisions)
+            if np_collision_count < collision_agent_freq[atf] or (np_collision_count == collision_agent_freq[atf] and np_cost < result_costs[atf]):
+                #replace path with better one, reset missCount
+                miss_count = 0
 
-        # 4. Reevaluate the solution, if better, keep it
+                #adjust collision trackers
+                collisions -= (collision_agent_freq[atf]-np_collision_count)
+                collision_agent_freq[atf] = np_collision_count
+                for snth in range(self.num_of_agents):
+                    collision_pair_freq[atf][snth] = np_collisions[snth]
+                    collision_agent_freq[snth] -= (collision_pair_freq[snth][atf]-np_collisions[snth])
+                    collision_pair_freq[snth][atf] = np_collisions[snth]
 
+                #replace path and its cost
+                result[atf] = new_path
+                result_costs[atf] = cost_of_path(new_path)
+                    
+
+            else: #not better, increase missCount
+                miss_count += 1
 
 
         # 5. If no improvment is found after some number of attempts, return solution
@@ -167,11 +155,14 @@ class LargeNeighbourhoodSolver(object):
         self.CPU_time = timer.time() - start_time
 
         print("\n Found a solution! \n")
+        print("Number of Collisions:   {}".format(collisions))
         print("CPU time (s):    {:.2f}".format(self.CPU_time))
         print("Sum of costs:    {}".format(get_sum_of_cost(result)))
-        #print("Paths in the solution:")
-        #print_paths(result)
-        #for j in range(self.num_of_agents):
-            #print("Agent #"+str(j),result[j])
-        #print(result)
+        """
+        print("Paths in the solution:")
+        print_paths(result)
+        for j in range(self.num_of_agents):
+            print("Agent #"+str(j),result[j])
+        print(result)
+        """
         return result
